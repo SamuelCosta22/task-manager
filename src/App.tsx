4b41eb4d0466b02fbff234af51eed634ae0c6e24
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2025-05-22 14:32:46 -0300

    chore: add tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1>Hello world!!!</h1>;
+  return (
+    <h1 className="bg-red-500 font-bold text-4xl">
+      <span className="text-green-500 font-light">Hello</span> world!!!
+    </h1>
+  );
 }
 
 export default App;
