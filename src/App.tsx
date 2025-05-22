9824758842c5324e25111627f73c4abda0bfb105
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2025-05-22 15:46:28 -0300

    chore: add poppins font

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <h1 className="bg-red-500 font-bold text-4xl">
-      <span className="text-green-500 font-light">Hello</span> world!!!
+      <span className="text-green-500 text-3xl">Hello</span> world!!!
     </h1>
   );
 }
