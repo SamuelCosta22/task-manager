a6b026455ef84a3800c54a4d2f76220286eb9364
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2025-05-22 15:48:49 -0300

    chore: add tailwind prettier plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <h1 className="bg-red-500 font-bold text-4xl">
-      <span className="text-green-500 text-3xl">Hello</span> world!!!
+    <h1 className="mt-2 bg-red-500 text-4xl font-bold">
+      <span className="text-3xl text-green-500">Hello</span> world!!!
     </h1>
   );
 }
